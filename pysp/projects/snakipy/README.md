db4471f4822cfd9007779bf63d8c5a3656d1046d
joserequenaidv <joserequenaidv@gmail.com>
2020-07-10 12:58:15 +0100

    :space_invader: Add snakipy project, init

---
+++ pysp/projects/snakipy/README.md
@@ -0,0 +1,16 @@
+# Snakipy
+This is my project for the Videogames module of the [Python Specialization Course](https://github.com/joserequenaidv/my-eoi/blob/master/pysp/README.md).
+
+## Links
+- [Videogames](https://github.com/joserequenaidv/my-eoi/blob/master/pysp/11-videogames/README.md) 
+
+## Project Journal
+### Introduction
+
+### Troubleshooting the Project
+
+### Challenges on the Way
+
+### Acquired Skills
+
+### What's next
---
+++ pysp/projects/snakipy/settings.py
@@ -0,0 +1,20 @@
+# define some colors (R, G, B)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+LIGHTBROWN = (175, 126, 47)
+
+# game settings
+WIDTH = 480   # 16 * 64 or 32 * 32 or 64 * 16
+HEIGHT = 480  # 16 * 48 or 32 * 24 or 64 * 12
+FPS = 30
+TITLE = "Tilemap Demo"
+BGCOLOR = LIGHTBROWN
+
+TILESIZE = 16
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
