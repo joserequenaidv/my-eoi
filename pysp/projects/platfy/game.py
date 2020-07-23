374df2cccdbe67ff2fff83e54a3a8bf09e256620
joserequenaidv <joserequenaidv@gmail.com>
2020-07-23 17:08:19 +0100

    :space_invader: Add new project: platform game init

---
+++ pysp/projects/platfy/game.py
@@ -0,0 +1,29 @@
+import pygame
+
+from settings import *
+
+class Game:
+    def __init__(self):
+        pass
+
+    def load_data(self):
+        pass
+
+    def start_game(self):
+        pass
+
+    def run(self):
+        pass
+
+    def events(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+if __name__ == '__main__':
+
+    game = Game()
